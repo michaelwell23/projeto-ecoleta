20bd693fdd9ccfe3aef90de00ec3f21a7f3b730d
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-06-16 19:21:43 -0300

    listando os items de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-  return response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 const PORT = 3333;
 app.listen(PORT, () => {
