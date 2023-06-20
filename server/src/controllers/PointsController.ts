c10ac577497ba26e4c62a4f9b7850a11d002789a
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-06-20 16:00:40 -0300

    Criando listagem de pontos (filtrando por estado/ cidade/ item)

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,7 +1,25 @@
-import { request, response } from 'express';
+import { query, request, response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request, response) {
+    const { city, uf, items } = request.query;
+
+    const parssedItems = String(items)
+      .split(', ')
+      .map((item) => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parssedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   async show(request, response) {
     const { id } = request.params;
 
@@ -48,6 +66,8 @@ class PointsController {
     });
 
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
 
     return response.json({
       id: point_id,
