52d2c7e476235f6260187a6e275345d669f9a53f
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-06-21 18:05:22 -0300

    Limpando estrutura do projeto e construindo página home do projeto

---
+++ web/src/Pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import './Home.css';
+
+import { FiLogIn } from 'react-icons/fi';
+
+import logo from '../assets/logo.svg';
+
+const Home = () => {
+  return (
+    <div id='page-home'>
+      <div className='content'>
+        <header>
+          <img src={logo} alt='Ecoleta' />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+
+          <a href='/create-point'>
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
