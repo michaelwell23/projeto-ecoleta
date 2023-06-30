de66b58fe9c0293dd97389a6f514d1abfd79e673
Michael Wellington Lopes <michael23.wellington@gmail.com>
2023-06-30 16:50:57 -0300

    criando página do mapa

---
+++ mobile/src/pages/Create-points/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CreatePoints = () => {
+  return <View />;
+};
+
+export default CreatePoints;
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './pages/Home';
 import Points from './pages/Points';
 import Detail from './pages/Detail';
+import CreatePoints from './pages/Create-points';
 
 const AppStack = createStackNavigator();
 
@@ -18,6 +19,7 @@ const Routes = () => {
         <AppStack.Screen name='Home' component={Home} />
         <AppStack.Screen name='Points' component={Points} />
         <AppStack.Screen name='Detail' component={Detail} />
+        <AppStack.Screen name='CreatePoints' component={CreatePoints} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
